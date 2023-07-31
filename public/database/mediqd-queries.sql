d903a1a83719ccd8bc6de210acbe164817a21644
Saksham Kumar <sakshamkumar98766@gmail.com>
2023-07-31 12:12:37 +0530

    Added all the MySQL database queries required for functioning

---
+++ public/database/mediqd-queries.sql
@@ -0,0 +1,32 @@
+# ---------------------------------------------------- Run these query to create database and use them ----------------------------------------------------
+create database signupform;
+use signupform;
+
+
+# ---------------------------------------------------- Creating table to store users login data ----------------------------------------------------
+create table users1(email varchar(50) primary key, pwd varchar(20), type varchar(10), dos date, status int);
+select * from users1;
+delete from users1;
+
+
+# ---------------------------------------------------- Creating table to store donor profile data ----------------------------------------------------
+create table donors(email varchar(100) primary key, name varchar(30), mobile varchar(12), address varchar(100),city varchar(20), proof varchar(20), pic varchar(200), ahours varchar(20));
+select * from donors;
+delete from donors;
+
+
+# ---------------------------------------------------- Creating table to store needy profile data ----------------------------------------------------
+create table needy(email varchar(100) primary key, name varchar(30), mobile varchar(12), dob date, gender varchar(10), city varchar(20), address varchar(100), pic varchar(200));
+select * from needy;
+delete from needy;
+
+
+# ---------------------------------------------------- Creating table to store available medicines data ----------------------------------------------------
+create table medsavailable(id int AUTO_INCREMENT primary key, email varchar(50), med varchar(50), expdate date, packing varchar(20), qty int);
+select * from medsavailable;
+delete from medsavailable;
+
+
+# ---------------------------------------------------- Run these queries if you want to delete the database or a table ----------------------------------------------------
+# DROP DATABASE signupform;
+# DROP TABLE {table_name};
